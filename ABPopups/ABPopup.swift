630ff13b9a7add49c01b8aa234b9bd73a7650bdc
Artur Bartczak <artur.bartczak@kinguin.io>
2018-07-19 08:32:17 +0200

    Initial commit

---
+++ ABPopups/ABPopup.swift
@@ -0,0 +1,87 @@
+//
+//  ABPopup.swift
+//  ABPopups
+//
+//  Created by Artur Bartczak on 26.06.2018.
+//  Copyright © 2018 Artur Bartczak. All rights reserved.
+//
+
+import UIKit
+
+public class ABPopup: UIView {
+
+    let nibName = "ABPopup"
+    var contentView: UIView!
+    var timer: Timer?
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var visualEffectView: UIVisualEffectView!
+    
+    // MARK: Set Up View
+    public override init(frame: CGRect) {
+        // For use in code
+        super.init(frame: frame)
+        setUpView()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        // For use in Interface Builder
+        super.init(coder: aDecoder)
+        setUpView()
+    }
+    
+    // Allow view to control itself
+    public override func layoutSubviews() {
+        // Rounded corners
+        self.layoutIfNeeded()
+        self.contentView.layer.masksToBounds = true
+        self.contentView.clipsToBounds = true
+        self.contentView.layer.cornerRadius = 10
+    }
+    
+    public override func didMoveToSuperview() {
+        // Fade in when added to superview
+        // Then add a timer to remove the view
+        self.contentView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        UIView.animate(withDuration: 0.15, animations: {
+            self.contentView.alpha = 1.0
+            self.contentView.transform = CGAffineTransform.identity
+        }) { _ in
+            self.timer = Timer.scheduledTimer(
+                timeInterval: TimeInterval(3.0),
+                target: self,
+                selector: #selector(self.removeSelf),
+                userInfo: nil,
+                repeats: false)
+        }
+    }
+    @objc private func removeSelf() {
+        // Animate removal of view
+        UIView.animate(
+            withDuration: 0.15,
+            animations: {
+                self.contentView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+                self.contentView.alpha = 0.0
+        }) { _ in
+            self.removeFromSuperview()
+        }
+    }
+    
+    private func setUpView() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: self.nibName, bundle: bundle)
+        self.contentView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        addSubview(contentView)
+        
+        contentView.center = self.center
+        contentView.autoresizingMask = []
+        contentView.translatesAutoresizingMaskIntoConstraints = true
+        
+        titleLabel.text = ""
+        contentView.alpha = 0.0
+    }
+    
+    public func set(title text: String) {
+        self.titleLabel.text = text
+    }
+}
---
+++ ABPopups/ABPopups.h
@@ -0,0 +1,19 @@
+//
+//  ABPopups.h
+//  ABPopups
+//
+//  Created by Artur Bartczak on 26.06.2018.
+//  Copyright © 2018 Artur Bartczak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ABPopups.
+FOUNDATION_EXPORT double ABPopupsVersionNumber;
+
+//! Project version string for ABPopups.
+FOUNDATION_EXPORT const unsigned char ABPopupsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ABPopups/PublicHeader.h>
+
+
